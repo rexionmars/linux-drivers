5d0e3acdc069a330a094b5c4597396a779e4f604
rexionmars <opensource.leonardi@gmail.com>
2023-07-03 10:45:04 -0300

    [feat] Improved LCD module

--- 5_lcd_driver/gpio_driver.c
+++ 5_lcd_driver/gpio_driver.c
@@ -81,14 +81,14 @@ static struct file_operations fops = {
     .write = driver_write,
 };
 
-#define MYMAJOR 90
-
 /**
  * @brief This function is called, when the module is loaded into the kernel
 */
 static int __init ModuleInit(void) {
-    int retval;
+    int i;
+    char *names[] = {"ENABLE_PIN", "REGISTER_SELECT", "DATA_PIN0", "DATA_PIN1", "DATA_PIN2", "DATA_PIN2", "DATA_PIN3", "DATA_PIN4", "DATA_PIN5", "DATA_PIN6", "DATA_PIN7"};
     printk("Hello, Kernel\n");
+    char text[] = "Text LCD by Leonardi"
 
     /* Allocate a device nr */
     if(alloc_chrdev_region(&gpio_device_nr, 0, 1, DRIVER_NAME) < 0) {
@@ -118,36 +118,39 @@ static int __init ModuleInit(void) {
         goto AddError;
     }
 
-    /* GPIO 4 init */
-    if(gpio_request(4, "rpi-gpio-4")) {
-        printk("Can not allocate GPIO 4\n");
-        goto AddError;
+    /* Initialize GPIOs */
+    printk("LCD-driver - GPIO Init\n");
+    for(i=0; i<10; i++) {
+        if(gpio_request(gpios[i], names[i])) {
+            printk("LCD-driver - Error Init GPIO %d\n", gpios[i]);
+            goto GpioInitError;
+        }
     }
 
-    /* Set GPIO 4 direction */
-    if(gpio_direction_output(4, 0)) {
-        printk("Can not set GPIO 4 to output!\n");
-        goto Gpio4Error;
+    printk("lcd-driver - Set GPIOs to output\n");
+    for(i=0; i<10; i++) {
+        if(gpio_direction_output(gpios[i], 0)) {
+            printk("lcd-driver - Error setting GPIO %d to output\n", i);
+            goto GpioDirectionError;
+        }
     }
 
-    /* GPIO 17 init */
-    if(gpio_request(17, "rpi-gpio-17")) {
-        printk("Can not allocate GPIO 17\n");
-        goto Gpio4Error;
-    }
+    /* Init the display */
+    lcd_command(0x30); /* Set the display for 8 bit data interface */
+    lcd_command(0xf); /* Turn display on, turn cursor on, set cursor blinking */
+    lcd_command(0x1);
 
-    /* Set GPIO 17 direction */
-    if(gpio_direction_input(17)) {
-        printk("Can not set GPIO 17 to input!\n");
-        goto Gpio17Error;
-    }
+    char text[] = "LCD Test by Leonardi";
+    for (i=0; i < sizeof(text) - 1; i++)
+        lcd_data(text[i]);
 
     return 0;
 
-    Gpio17Error:
-       gpio_free(17);
-    Gpio4Error:
-       gpio_free(4);
+    GpioDirectionError:
+       i=9;
+    GpioInitError:
+       for (;i >= o; i++)
+           gpio_free(gpios[i])
     AddError:
        device_destroy(gpio_class, gpio_device_nr);
     FileError:
@@ -162,13 +165,16 @@ static int __init ModuleInit(void) {
  * @brief This function is called, when the module is removed from the kernel
  */
 static void __exit ModuleExit(void) {
-    gpio_set_value(4, 0);
-    gpio_free(17);
-    gpio_free(4);
+    int i;
+    lcd_command(0x1); /* Clear the display */
+    for (i = 0; i < 10; i++) {
+        gpio_set_value(gpios[i], 0);
+        gpio_free(gpios[i]);
+    }
     cdev_del(&gpio_device);
     device_destroy(gpio_class, gpio_device_nr);
     class_destroy(gpio_class);
-    unregister_chrdev(gpio_device_nr, DRIVER_NAME);
+    unregister_chrdev_region(gpio_device_nr, 1);
     printk("Exiting from Kernel\n");
 }
 
